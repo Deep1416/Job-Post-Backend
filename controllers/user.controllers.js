062f61de0f9bb7b083ce124077f7ab7bb38439d8
Deependra <deependra186037@gamil.com>
2024-08-05 15:47:17 +0530

    done

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -122,8 +122,9 @@ export const login = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
-        httpOnly: true,
-        sameSite: "strict",
+        // httpOnly: true,
+        sameSite: "none",
+        secure :true
       })
       .json({
         message: `Welcome, ${user.fullname}!`,
